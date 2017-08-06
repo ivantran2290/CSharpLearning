c3f2ddf1548cdf05c302d057bcaf13be03e1c84b
Ivan <ivantran2290@gmail.com>
2017-08-06 13:28:10 +0900

    #add

---
+++ LenhCoBan/DTO/StructData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LapTrinhCoBan.DTO
+{
+    struct NHANVIEN
+    {
+        public string hoTen;
+        public DateTime ngaySinh;
+        public int tuoi;
+    }
+    struct HOCSINH
+    {
+        public string hoTen;
+        public double diemToan;
+        public double diemVan;
+        public double diemAnh;
+        public double diemTB;
+    }
+    struct MATHANG
+    {
+        public string tenMatHang;
+        public string loaiMatHang;
+        public int soLuong;
+        public int donGia;
+        public double tongTien;
+    }
+}
